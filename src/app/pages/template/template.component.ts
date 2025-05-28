b8f2f943b7b9a23bfc359ab852b6d9cc049f3b49
zeyad <112281551+zalmajali@users.noreply.github.com>
2025-05-28 14:39:30 +0300

    16

--- src/app/pages/template/template.component.ts
+++ src/app/pages/template/template.component.ts
@@ -539,7 +539,7 @@ constructor(private androidPermissions: AndroidPermissions,private chooser: Choo
     let valOfArray  = JSON.stringify(this.arraySendToServer);
     const sendValues = {'mainUserName':this.mainUserName,'templateName':this.nameOfTem,'lang':this.languageOfTem,'userName':this.userName,'password':this.password,'apiKey':this.apiKey,'mobile':this.number,'sessionLogin':this.sessionLogin,'msgWa':this.allDataOfMessages,valOfArray};
     alert(this.filedata);
-    if(this.filedata!=undefined && this.filedata!=null && this.filedata!=""){
+    if(this.filedata){
       let fileName = "";
       let mimeType: string="";
       let sendValuesNew = {'mainUserName':this.mainUserName,'templateName':this.nameOfTem,'lang':this.languageOfTem,'userName':this.userName,'password':this.password,'apiKey':this.apiKey,'mobile':this.number,'sessionLogin':this.sessionLogin,'msgWa':this.allDataOfMessages,valOfArray,'linkType':this.linkType};
